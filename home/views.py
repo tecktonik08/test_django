b756282a8279f41f66103be922a96cf3a0682a60
qweruiop <superinssa@gmail.com>
2021-07-09 17:55:12 +0900

    django_practice

--- home/views.py
+++ home/views.py
@@ -3,4 +3,18 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 def index(request):
-    return HttpResponse('<h1>안녕하세요. HOI입니다.</h1>')
+    path = request.path
+    resultstr = ''
+    if path == '/home':
+        resultstr = '<h1>안녕하세요. HOME 입니다.</h1>'
+    else :
+        resultstr = '<h1>여기는 main 입니다.</h1>'
+    return HttpResponse(resultstr)
+
+def index01(request):
+    result = {'n':'HOI', 'f':'K'}
+    return render(request, 'index.html', context=result)
+
+def index02(request):
+    result = {'n':request.GET['n'], 'f':request.GET['f']}
+    return render(request, 'index.html', context=result)
